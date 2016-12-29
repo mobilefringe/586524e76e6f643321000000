eeaee582df923a19e08b4cd2e6f204a335c356de
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-12-29 11:14:56 -0500

    Marlborough Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -14,7 +14,7 @@ function init() {
     var feature_items = getFeatureList();
     var one_item = feature_items.slice(0,1);
     renderFeatureItems('#feature_item','#feature_item_template', one_item);
-    var two_items = feature_items.slice(1,3);
+    var two_items = feature_items;
     console.log(two_items);
     renderFeatureItems('#home_feature','#home_feature_template', two_items);
     
