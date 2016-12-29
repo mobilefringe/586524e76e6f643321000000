4dce29e9c1702d79c26d48f9e04929caa4924575
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-12-29 14:03:44 -0500

    Marlborough Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -592,7 +592,12 @@ function renderPosts(container, template, collection){
         } else {
             val.image_url = val.image_url;
         }
-
+        if(val.author.lenth <= 0) {
+            val.author = "Marlborough Mall"
+        } else {
+            val.author = val.author;
+        }
+        
         var published_on = moment(val.publish_date).tz(getPropertyTimeZone());
         val.publish_date = published_on.format("MMMM Do, YYYY");
         
