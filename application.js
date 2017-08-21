35a8cb1f75653024fb0527fb97bf5687cb756ab5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-08-21 14:16:13 -0400

    Marlborough Mall 1.1  /application.js

--- application.js
+++ application.js
@@ -526,11 +526,12 @@ function renderPostDetails(container, template, collection){
     var item_rendered = [];
     var template_html = $(template).html();
     $.each( collection , function( key, val ) {
-        // if (val.image_url != null) {
-        //     val.post_image = val.image_url;
-        // } else {
-        //     val.post_image = "//codecloud.cdn.speedyrails.net/sites/586524e76e6f643321000000/image/jpeg/1497450277000/marlborough_logo.jpg";
-        // }
+        if (val.image_url == null) {
+            val.post_image = "//codecloud.cdn.speedyrails.net/sites/586524e76e6f643321000000/image/jpeg/1497450277000/marlborough_logo.jpg";
+            
+        } else {
+            val.post_image = val.image_url;
+        }
         
         if (val.image_url.indexOf('missing.png') > -1) {
             val.image_url = "//codecloud.cdn.speedyrails.net/sites/586524e76e6f643321000000/image/jpeg/1497450277000/marlborough_logo.jpg";
