183c6d1624f5793ecb3907cfb530f1c74bb74e93
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-08-30 15:22:45 -0400

    Marlborough Mall 1.1  /application.js

--- application.js
+++ application.js
@@ -370,7 +370,7 @@ function renderPromoDetails(container, template, collection){
         }
         
         if(val.promo_image_url_abs.indexOf('missing.png') > -1 || val.promo_image_url_abs == null){
-            val.promo_image_show = "display:none";
+            val.promo_img_show = "display:none";
         }
         
         var show_date = moment(val.show_on_web_date);
